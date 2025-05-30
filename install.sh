22c70394e0223f66e708e446397154a195910a3c
Jorge Pulido Lopez <jorge.pulido@basf.com>
2025-05-30 13:41:58 +0200

    Install and sync ready

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Enable Rosetta 2 for Apple Silicon
+softwareupdate --install-rosetta --agree-to-license
+
 brew install font-fira-code-nerd-font
 
 # https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
@@ -10,14 +13,21 @@ brew install --cask font-fira-code
 # https://www.kcl-lang.io/docs/user_docs/getting-started/install#homebrew-macos-1
 brew install kcl-lang/tap/kcl-lsp
 
-# https://www.jetify.com/devbox/docs/installing_devbox/
+# https://www.jetify.com/docs/devbox/installing_devbox/?install-method=macos
 curl -fsSL https://get.jetify.com/devbox | bash
-
-# https://obsproject.com/kb/mac-installation#homebrew
-brew install --cask obs
-
 # https://github.com/hidetatz/kubecolor
 brew install kubecolor
 
 # https://github.com/sharkdp/bat
 brew install bat
+
+brew install podman-desktop
+
+brew install docker-credential-helper
+
+brew install azure-cli
+az extension add --name azure-devops
+
+brew install microsoft-azure-storage-explorer
+
+devbox run 
--- sync.sh
+++ sync.sh
@@ -1,12 +1,7 @@
-gcloud auth login
-
-teller env >.config/fabric/.env
-
 rm ~/.zshrc
 
 stow .
 
-echo "## Follow the instructions at https://github.com/tonsky/FiraCode/wiki/VS-Code-Instructions to enable Fira Code in VS Code" \
-    | gum format
+gum format
 
 echo '## Execute `source ~/.zshrc`.' | gum format
